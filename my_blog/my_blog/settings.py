2b8bdcfdd3e2276c2e49fd2044828813c0dd3230
shugui <shuguid@hotmail.com>
2018-04-16 23:52:00 +0800

    markdown and add a static folder

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'article' #新增的app名称
+    'article', #新增的app名称
+    'article.templatetags.custom_markdown' #自定义的markdown应用
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
